a59a71bfd7b9cada9e2dc46015e191592b522266
Jose Talens <jose.talens@williamhill.com>
2015-03-07 15:10:27 +0000

    Dot module. Returns a dot (graphviz) format based on an input dependency map

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
-    fs: require('./lib/fs'),
-    parser: require('./lib/parser'),
-    helper: require('./lib/helpers'),
-    graph: require('./lib/graph'),
-    builders: require('./lib/builders')
+	fs: require('./lib/fs'),
+	parser: require('./lib/parser'),
+	helper: require('./lib/helpers'),
+	graph: require('./lib/graph'),
+	builders: require('./lib/builders'),
+	layout: require('./lib/layout')
 };
