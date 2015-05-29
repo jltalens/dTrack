852d743ed9f9fae79c0fba035fff5c538c8954fa
Jose Talens <jose.talens@williamhill.com>
2015-05-29 08:51:12 +0100

    Added path to the file inputs

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@
     describe('dTrack', function(){
         describe('File module', function(){
             it('Should be able to return the file content', function(){
-                var content = dTrack.fs.open('./support/testFile.txt');
+                var content = dTrack.fs.open(__dirname + '/support/testFile.txt');
                 assert.equal(typeof content, 'string');
                 assert.equal(content[3], 'd');
             });
@@ -18,35 +18,35 @@
 
         describe('Parser module', function() {
             it('Should be able to return the tokens of a js file', function() {
-                var tokens = dTrack.parser.getTokens('./support/config.js');
+                var tokens = dTrack.parser.getTokens(__dirname + '/support/config.js');
                 assert.equal(Object.prototype.toString.call(tokens), '[object Array]');
                 assert(tokens[0].hasOwnProperty('type'));
                 assert(tokens[0].hasOwnProperty('value'));
             });
             describe('Get AMD modules dependencies', function() {
                 it('should be able to recieve the function name to process', function(){
-                    var tokens = dTrack.parser.getTokens('./support/config.js');
+                    var tokens = dTrack.parser.getTokens(__dirname + '/support/config.js');
                     var args = dTrack.parser.argumentsFor('requirejs', tokens);
                     assert(args instanceof Array);
                     assert.equal(args[0], 'global/globals');
                     assert.equal(args[1], 'global/config');
                 });
                 it('should be able to process regular amd modules', function(){
-                    var tokens = dTrack.parser.getTokens('./support/module.js');
+                    var tokens = dTrack.parser.getTokens(__dirname + '/support/module.js');
                     var args = dTrack.parser.argumentsFor('define', tokens);
                     assert(args instanceof Array);
                     assert.equal(args[0], 'moduleA');
                 });
 
                 it('should be able to process amd modules inside self-exec functions', function(){
-                    var tokens = dTrack.parser.getTokens('./support/moduleSelfFunc.js');
+                    var tokens = dTrack.parser.getTokens(__dirname + '/support/moduleSelfFunc.js');
                     var args = dTrack.parser.argumentsFor('define', tokens);
                     assert(args instanceof Array);
                     assert.equal(args[0], 'moduleA');
                 });
 
                 it('should be able to process non-array deps in amd modules', function(){
-                    var tokens = dTrack.parser.getTokens('./support/moduleNonArray.js');
+                    var tokens = dTrack.parser.getTokens(__dirname + '/support/moduleNonArray.js');
                     var args = dTrack.parser.argumentsFor('define', tokens);
                     assert(args instanceof Array);
                     assert.equal(args[0], 'moduleA');
@@ -58,7 +58,7 @@
             describe('requirejs functions', function(){
                 it('should be able to get the require paths from the keys', function(){
                     var dep = dTrack.builders.dependencyBuilder.getBuilder('AMD');
-                    var map = dep.requireMaps('./support/requireConfig.js');
+                    var map = dep.requireMaps(__dirname + '/support/requireConfig.js');
                     assert.equal(Object.keys(map).length, 32);
                     assert(map.hasOwnProperty('Backbone'));
                     assert.equal(map.Backbone,'vendor/backbone/backbone-1.1.2.min');
@@ -87,8 +87,8 @@
         describe('Graph module', function(){
             describe('AMD dependencies', function(){
                 it('should be able to accept a config file', function(){
-                    var dependencies = dTrack.graph.readFromFile('AMD','support/sampleAppConfig/app.js',
-                        'support/sampleAppConfig/config.js');
+                    var dependencies = dTrack.graph.readFromFile('AMD',__dirname + '/support/sampleAppConfig/app.js',
+                        __dirname + '/support/sampleAppConfig/config.js');
                     assert(dependencies.hasOwnProperty('app.js'));
                     assert(dependencies['app.js'].jquery);
                     assert(dependencies['app.js'].Router);
@@ -100,7 +100,7 @@
                     assert(dependencies['app.js'].PageController.Repository);
                 });
                 it('should be able to the first level dependencies', function(){
-                    var dependencies = dTrack.graph.readFromFile('AMD','support/sampleApp/app.js');
+                    var dependencies = dTrack.graph.readFromFile('AMD',__dirname + '/support/sampleApp/app.js');
                     //first level
                     assert(dependencies.hasOwnProperty('app.js'));
                     assert(dependencies['app.js'].jquery);
@@ -128,8 +128,8 @@
         describe('Dot integration', function() {
            describe('Dot module basic layout', function() {
                it('should write output in doc format', function() {
-                   var dependencies = dTrack.graph.readFromFile('AMD', 'support/sampleAppConfig/app.js',
-                    'support/sampleAppConfig/config.js');
+                   var dependencies = dTrack.graph.readFromFile('AMD', __dirname + '/support/sampleAppConfig/app.js',
+                    __dirname + '/support/sampleAppConfig/config.js');
                    var output = 'digraph dependencies { "app.js" -> "jquery";"app.js" -> "Router";' +
                            '"app.js" -> "PageController";' +
                            '"app.js" -> "BasePage";' +
@@ -151,7 +151,7 @@
         });
 
         describe('CommonJS module', function(){
-            it('should ')
+            //it('should ')
         });
     });
 })();
