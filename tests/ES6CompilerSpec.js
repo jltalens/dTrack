99f6e26d703b0697ed5f44e44be3ec8c51e6dc98
Jose Talens <jltalens@hotmail.com>
2015-09-06 17:37:48 +0100

    Configured to compile and pass tests in ES6 format

---
+++ tests/ES6CompilerSpec.js
@@ -0,0 +1,16 @@
+describe("ES6 Compiling", function(){
+    "use strict";
+    var assert = require("assert");
+    it("should compile ES6 classes", function () {
+        class Fraction {
+            constructor(number) {
+                this.number = number;
+            }
+            update(number) {
+                this.number = number;
+            }
+        }
+        var x = new Fraction(5);
+        assert(typeof x === "object");
+    });
+});
